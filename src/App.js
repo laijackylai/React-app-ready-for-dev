b9da94cced870df6975eeca774cdf5c7bb93428f
laijackylai <laijackylai@gmail.com>
2021-05-13 08:04:21 +0000

    update app.js

--- src/App.js
+++ src/App.js
@@ -3,8 +3,8 @@ import React from 'react';
 export default function App() {
   return (
     <>
-  <div>Hello World</div>
-  <div>This is a test page</div>
-  </>
+      <div>Hello World</div>
+      <div>This is a test page</div>
+    </>
   );
 }
